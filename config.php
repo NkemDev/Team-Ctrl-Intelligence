4cf7b53fb5e89a45ac9fe2cc5a09de19c524c18a
nic121 <32997273+nic121@users.noreply.github.com>
2019-09-21 02:50:49 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'audacity');
+define('DB_USERNAME', 'id10950145_ctrlintelligence');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'id10950145_audacity');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -14,3 +14,4 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+
